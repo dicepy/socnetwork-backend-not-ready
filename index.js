e09d247618a404d5a8a9009769451de1af280ba3
dicepy <mdi.2005@mail.ru>
2023-10-16 19:13:18 +0300

    mvc, crud etc

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 import express from 'express';
+import mongoose from "mongoose";
+import checkAuth from './utils/checkAuth.js'
+import {registerValidation, loginValidation, postCreateValidation} from "./validations.js";
+import * as UserController from './controllers/UserController.js'
+import * as PostController from './controllers/PostController.js'
+import {check} from "express-validator";
+
+mongoose.connect('mongodb+srv://admin:6w6w6w@clustersn.anmxqvc.mongodb.net/blog?retryWrites=true&w=majority',
+    ).then(() => {
+        console.log('DB OK');
+    }).catch((err) => {
+        console.log('Db Error', err);
+    });
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-})
+app.post('/auth/login', loginValidation,UserController.login);
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
-    res.json({
-        success: true,
-    })
-});
+app.post('/auth/register', registerValidation, UserController.register);
+
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts',checkAuth ,postCreateValidation , PostController.create);
+// app.delete('/posts', PostController.remove);
+// app.patch('/posts', PostController.update);
+
 
 app.listen(4444, (err) => {
     if (err){
